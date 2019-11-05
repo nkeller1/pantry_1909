ba109e2c3afcc2d70a098462df083290d4ceeedc
Nathan Keller <keller.nathan@gmail.com>
2019-11-05 11:28:08 -0700

    still stuck on restock. tried map.

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -6,25 +6,16 @@ class Pantry
     @stock_check = 0
   end
 
-
   def stock_check(ingredient)
     @stock_check
   end
 
   def restock(ingredient, amount)
     @stock[:ingredient] = amount
-    amt = []
-    @stock.values.each do |add|
+    items = @stock.values.map do |add|
       require "pry"; binding.pry
-      amt << add
+      add
     end
-
-    # items = []
-    # add = @stock[:ingredient] = amount
-    # items << add
-    # @stock[:ingredient] = amount
-    # items << add + @stock.values
-
   end
 
 end
