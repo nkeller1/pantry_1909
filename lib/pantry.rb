912e7d087e32327720705df95ff9bdb1c762d5a1
Nathan Keller <keller.nathan@gmail.com>
2019-11-05 11:22:42 -0700

    I have tried each i have tried .sum. The values of the hash for @stock just keep getting over ridden.

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -6,20 +6,24 @@ class Pantry
     @stock_check = 0
   end
 
-  # def stock(ingredient, amount)
-  #   @stock[:ingredient] = amount
-  # end
 
   def stock_check(ingredient)
     @stock_check
   end
 
   def restock(ingredient, amount)
-    items = []
-    add = @stock[:ingredient] = amount
-    items << add
     @stock[:ingredient] = amount
-    items << add + @stock.values
+    amt = []
+    @stock.values.each do |add|
+      require "pry"; binding.pry
+      amt << add
+    end
+
+    # items = []
+    # add = @stock[:ingredient] = amount
+    # items << add
+    # @stock[:ingredient] = amount
+    # items << add + @stock.values
 
   end
 
